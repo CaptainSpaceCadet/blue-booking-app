f375e701cd822ab26c44f771079090acdce3cfe3
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2026-07-12 13:32:34 +0900

    feat: initial demo of partial all-auth forms
    #8

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render
 
 from apps.accounts.models import UserProfile
-
 
 class CustomSignupForm(forms.Form):
     display_name = forms.CharField(
@@ -16,3 +18,10 @@ class CustomSignupForm(forms.Form):
         profile = UserProfile.objects.create(user=user)
         profile.display_name = self.cleaned_data["display_name"]
         profile.save()
+
+class ChangeDisplayNameForm(forms.Form):
+    display_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={"placeholder": "Display Name"}),
+    )
+
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,4 +6,9 @@ app_name = "accounts"
 urlpatterns = [
     path("open-view/", views.open_view, name="open_view"),
     path("secret-view/", views.secret_view, name="secret_view"),
+    path(
+        "password-change-partial/",
+        views.CustomPasswordChangeView.as_view(),
+        name="password_change_partial",
+    ),
 ]
