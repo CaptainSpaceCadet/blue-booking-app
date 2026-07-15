e00c421c92d0558f7bf172144a7177cadfe428a1
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2026-07-15 20:21:04 +0900

    feat: implement campaign-page
    Implement campaign home page and the html view that serves it.
    
    #13

--- apps/campaigns/urls.py
+++ apps/campaigns/urls.py
@@ -6,4 +6,5 @@ app_name = "campaigns"
 urlpatterns = [
     path("campaign-list/", views.CampaignListView.as_view(), name="campaign-list"),
     path("create-campaign/", views.create_campaign, name="create-campaign"),
+    path("<int:id>/", views.campaign_page, name="campaign-page"),
 ]
