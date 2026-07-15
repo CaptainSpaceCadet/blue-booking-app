301c15a84206e44cdf6c13916e4742e6e5947478
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2026-07-15 18:16:49 +0900

    fix: change urls to have / at the end
    #13

--- apps/campaigns/urls.py
+++ apps/campaigns/urls.py
@@ -4,6 +4,6 @@ from apps.campaigns import views
 app_name = "campaigns"
 
 urlpatterns = [
-    path("campaign-list", views.CampaignListView.as_view(), name="campaign-list"),
-    path("create-campaign", views.create_campaign, name="create-campaign"),
+    path("campaign-list/", views.CampaignListView.as_view(), name="campaign-list"),
+    path("create-campaign/", views.create_campaign, name="create-campaign"),
 ]
