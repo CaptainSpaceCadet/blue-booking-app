2b69fa57b7f8f2513eaebc61171dba59f17a0f1f
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2026-07-23 10:10:36 +0900

    fix: make display name non-blank
    #14

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-    display_name = models.CharField(blank=True, null=False, max_length=100)
+    display_name = models.CharField(blank=False, null=False, max_length=100)
 
     campaigns = models.ManyToManyField(
         "campaigns.Campaign",
